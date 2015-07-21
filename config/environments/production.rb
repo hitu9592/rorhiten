5a34f750326a2701837b9fd9ff74ae407d09d30a
HITEN CHAUHAN <hitu9592@gmail.com>
2015-07-22 02:55:59 +0530

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'http://hiten-hitenror.rhcloud.com' }
+  config.action_mailer.asset_host = 'http://hiten-hitenror.rhcloud.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'hiten9592@gmail.com',
+    password: '9601198035,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
